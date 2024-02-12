6b3c36e375abed15b22e8cd4ce8209c022e0ae96
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-02-12 11:32:47 +0500

    added courses command

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 TOKEN = '6081271530:AAG3HLfJVIZ-tFehtEdNL50F_UdNxkDt74Y'
+
+courses = {
+    'USD': 0,
+    'EUR': 0,
+    'RUB': 0
+}
--- handlers/cmd_handlers.py
+++ handlers/cmd_handlers.py
@@ -1,7 +1,9 @@
+import requests
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from aiogram.enums import ParseMode
 from aiogram import Router
+
+from config import courses
 
 cmd_router = Router()
 
@@ -10,4 +12,25 @@ async def cmd_start(message: Message):
     txt = ("""Hello, 
             welcome to ExchangeRate Bot!
             for help send /help command to us""")
+    await message.answer(text=txt)
+
+@cmd_router.message(Command('help'))
+async def cmd_help(message: Message):
+    txt = """
+        /courses - know exchange rates
+        /dollar - know the dollar exchange rate
+        /euro - know the euro exchange rate
+        /rouble - know the rouble exchange rate
+        
+        Send an amount and the bot will return it to you in different currencies"""
+    await message.answer(text=txt)
+
+@cmd_router.message(Command('courses'))
+async def cmd_courses(message: Message):
+    response = requests.get('https://cbu.uz/uz/arkhiv-kursov-valyut/json/')
+    txt = "Today's exchange rates:\n"
+    for course in response.json():
+        if course['Ccy'] in ['USD', 'EUR', 'RUB']:
+            courses[course['Ccy']] = float(course['Rate'])
+            txt += f"1 {course['CcyNm_EN']} - {course['Rate']} so'm\n"
     await message.answer(text=txt)
